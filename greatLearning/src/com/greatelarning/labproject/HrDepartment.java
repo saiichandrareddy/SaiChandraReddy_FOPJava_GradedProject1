862015332a9f0cfd0ffecaf209bae7bacf271a6b
Sai chandra reddy Katukuri <110294689+saiichandrareddy@users.noreply.github.com>
2022-10-08 21:20:59 +0530

    Add files via upload

---
+++ greatLearning/src/com/greatelarning/labproject/HrDepartment.java
@@ -0,0 +1,16 @@
+package com.greatelarning.labproject;
+
+public class HrDepartment extends SuperDepartment{
+	public String departmentName() {
+		return "HR Department";
+	}
+	public String getTodaysWork() {
+		return "Fill today’s timesheet and mark your attendance";
+	}	
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
